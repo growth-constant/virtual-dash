e93d3ee45bab26abcb61b73d554d7d88b8946766
LuisGrwtCnst <luis@growthconstant.com>
2021-04-13 16:03:03 -0600

    Personal leaderboard - Push the position data to the position_log variable

--- app/services/personal_leaderboard.rb
+++ app/services/personal_leaderboard.rb
@@ -36,32 +36,33 @@ class PersonalLeaderboard
     end
   end
 
-  ### Steps to get segment activity ###
-  # [/] 1 - Get the all user tries
-  # [/] 2 - Cycle throught the array
-  # [/] 3 - Get the date of i and i+1
-  # [/] 2 - Get overall leaderboard between the dates
-  # [] 3 - Save position on @position_log
-  # [] 4 - Repeat until finish compleate the array
   def segment_activity
     @tries = RaceTry.user_segments(@me, @leaderboard[:race_segment])
     @position_log = []
 
     @tries.each_with_index do | try, index |
       if not @tries.to_a[index + 1].nil?
+
         @date_leaderboard = RaceTry.tries_between_dates(
           @leaderboard[:race_segment],
           try[:start],
           @tries.to_a[index + 1][:start]
         )
-        @position_log.push(@date_leaderboard.index { |competitor| competitor.user_id == @me.id })
+        
+        @date_leaderboard.each_with_index do | race_try, index |
+          if race_try.user_id == @me.id
+            @position_log.push(
+              position: index,
+              date: race_try.start,
+              race_try_id: race_try.id
+            )
+          end
+        end
+
       end
     end
 
-    puts "### POSITION LOG ###"
-    puts @position_log
-    puts "######"
-
+    @position_log = @position_log.uniq { |log| log[:race_try_id]}
   end
 
 end
