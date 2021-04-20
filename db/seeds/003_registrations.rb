24f2e817b2590f0fab902aa34d11b459cb93ba94
LuisGrwtCnst <luis@growthconstant.com>
2021-04-20 17:04:32 -0600

    DB Seeders - Working on registrations seeder

--- db/seeds/003_registrations.rb
+++ db/seeds/003_registrations.rb
@@ -1,83 +1,82 @@
 # Users
-wilford = User.find_by name: 'Joseph Wilford'
-melanie = User.find_by name: 'Melanie Cavill'
-layton = User.find_by name: 'Andre Layton'
-josie = User.find_by name: 'Josie Wellstead'
-
+wilford = User.find_by email: 'mr.wilford@wilford.co'
+melanie = User.find_by email: 'melaniec@wilford.co'
+layton = User.find_by email: 'andre@thetail.xyz'
+josie = User.find_by email: 'josie@thetail.xyz'
 
 # Races
 snowpiercer_race = Race.find_by title: 'Snowpiercer'
 big_alice_race = Race.find_by title: 'Big Alice Race'
 
+puts "Data thingy"
+
+puts snowpiercer_race[:id]
+puts layton[:id]
+
 # Registrations
-Registration.find_or_create_by(
-  race_id: snowpiercer_race,
-  user_id: wilford,
-  status: 'registered',
-  payment_status: 'paid',
-  agreements_signed: true,
-  session_id: 'wilford-snowpiercer'
-)
-
-Registration.find_or_create_by(
-  race_id: snowpiercer_race,
-  user_id: melanie,
-  status: 'registered',
-  payment_status: 'paid',
-  agreements_signed: true,
-  session_id: 'melanie-snowpiercer'
-)
-
-Registration.find_or_create_by(
-  race_id: snowpiercer_race,
-  user_id: layton,
-  status: 'registered',
-  payment_status: 'paid',
-  agreements_signed: true,
-  session_id: 'layton-snowpiercer'
-)
-
-Registration.find_or_create_by(
-  race_id: snowpiercer_race,
-  user_id: josie,
-  status: 'registered',
-  payment_status: 'paid',
-  agreements_signed: true,
-  session_id: 'josie-snowpiercer'
-)
-
-Registration.find_or_create_by(
-  race_id: big_alice_race,
-  user_id: wilford,
-  status: 'registered',
-  payment_status: 'paid',
-  agreements_signed: true,
-  session_id: 'wilford-bigalice'
-)
-
-Registration.find_or_create_by(
-  race_id: big_alice_race,
-  user_id: melanie,
-  status: 'registered',
-  payment_status: 'paid',
-  agreements_signed: true,
-  session_id: 'melanie-bigalice'
-)
-
-Registration.find_or_create_by(
-  race_id: big_alice_race,
-  user_id: layton,
-  status: 'registered',
-  payment_status: 'paid',
-  agreements_signed: true,
-  session_id: 'layton-bigalice'
-)
-
-Registration.find_or_create_by(
-  race_id: big_alice_race,
-  user_id: josie,
-  status: 'registered',
-  payment_status: 'paid',
-  agreements_signed: true,
-  session_id: 'josie-bigalice'
-)
+Registration.create!([
+  {
+    race_id: snowpiercer_race,
+    user_id: wilford,
+    status: 'registered',
+    payment_status: 'paid',
+    agreements_signed: true,
+    session_id: 'wilford-snowpiercer'  
+  },
+  {
+    race_id: snowpiercer_race,
+    user_id: melanie,
+    status: 'registered',
+    payment_status: 'paid',
+    agreements_signed: true,
+    session_id: 'melanie-snowpiercer'
+  },
+  {
+    race_id: snowpiercer_race,
+    user_id: layton,
+    status: 'registered',
+    payment_status: 'paid',
+    agreements_signed: true,
+    session_id: 'layton-snowpiercer'
+  },
+  {
+    race_id: snowpiercer_race,
+    user_id: josie,
+    status: 'registered',
+    payment_status: 'paid',
+    agreements_signed: true,
+    session_id: 'josie-snowpiercer'
+  },
+  {
+    race_id: big_alice_race,
+    user_id: wilford,
+    status: 'registered',
+    payment_status: 'paid',
+    agreements_signed: true,
+    session_id: 'wilford-bigalice'
+  },
+  {
+    race_id: big_alice_race,
+    user_id: melanie,
+    status: 'registered',
+    payment_status: 'paid',
+    agreements_signed: true,
+    session_id: 'melanie-bigalice'
+  },
+  {
+    race_id: big_alice_race,
+    user_id: layton,
+    status: 'registered',
+    payment_status: 'paid',
+    agreements_signed: true,
+    session_id: 'layton-bigalice'
+  },
+  {
+    race_id: big_alice_race,
+    user_id: josie,
+    status: 'registered',
+    payment_status: 'paid',
+    agreements_signed: true,
+    session_id: 'josie-bigalice'
+  },
+])
--- db/seeds/004_race_tries.rb
+++ db/seeds/004_race_tries.rb
@@ -1,8 +1,12 @@
 # Users
-wilford = User.find_by name: 'Joseph Wilford'
-melanie = User.find_by name: 'Melanie Cavill'
-layton = User.find_by name: 'Andre Layton'
-josie = User.find_by name: 'Josie Wellstead'
+wilford = User.find_by email: 'mr.wilford@wilford.co'
+melanie = User.find_by email: 'melaniec@wilford.co'
+layton = User.find_by email: 'andre@thetail.xyz'
+josie = User.find_by email: 'josie@thetail.xyz'
+
+# Races
+snowpiercer_race = Race.find_by title: 'Snowpiercer'
+big_alice_race = Race.find_by title: 'Big Alice Race'
 
 # Registrations
 wilford_snowpiercer = Registration.find_by session_id: 'wilford-snowpiercer'
@@ -15,90 +19,85 @@ layton_big_alice = Registration.find_by session_id: 'layton-bigalice'
 josie_big_alice = Registration.find_by session_id: 'josie-bigalice'
 
 # Race tries
-RaceTry.find_or_create_by(
-  registration_id: wilford_snowpiercer,
-  user_id: wilford,
-  duration: 500,
-  start: '2021-04-19 00:00:00',
-  moving_time: 260,
-  start_date_local: '2021-04-19 00:00:00',
-  race_id: 1,
-  race_try_id: 1
-)
-
-RaceTry.find_or_create_by(
-  registration_id: melanie_snowpiercer,
-  user_id: melanie,
-  duration: 510,
-  start: '2021-04-19 00:00:00',
-  moving_time: 270,
-  start_date_local: '2021-04-19 00:00:00',
-  race_id: 1,
-  race_try_id: 2
-)
-
-RaceTry.find_or_create_by(
-  registration_id: layton_snowpiercer,
-  user_id: layton,
-  duration: 520,
-  start: '2021-04-19 00:00:00',
-  moving_time: 280,
-  start_date_local: '2021-04-19 00:00:00',
-  race_id: 1,
-  race_try_id: 3
-)
-
-RaceTry.find_or_create_by(
-  registration_id: josie_snowpiercer,
-  user_id: josie,
-  duration: 530,
-  start: '2021-04-19 00:00:00',
-  moving_time: 290,
-  start_date_local: '2021-04-19 00:00:00',
-  race_id: 1,
-  race_try_id: 4
-)
-
-RaceTry.find_or_create_by(
-  registration_id: wilford_big_alice,
-  user_id: wilford,
-  duration: 500,
-  start: '2021-04-19 00:00:00',
-  moving_time: 260,
-  start_date_local: '2021-04-19 00:00:00',
-  race_id: 2,
-  race_try_id: 5
-)
-
-RaceTry.find_or_create_by(
-  registration_id: melanie_big_alice,
-  user_id: melanie,
-  duration: 510,
-  start: '2021-04-19 00:00:00',
-  moving_time: 270,
-  start_date_local: '2021-04-19 00:00:00',
-  race_id: 2,
-  race_try_id: 6
-)
-
-RaceTry.find_or_create_by(
-  registration_id: layton_big_alice,
-  user_id: layton,
-  duration: 520,
-  start: '2021-04-19 00:00:00',
-  moving_time: 280,
-  start_date_local: '2021-04-19 00:00:00',
-  race_id: 2,
-  race_try_id: 7
-)
-
-RaceTry.find_or_create_by(
-  registration_id: josie_big_alice,
-  user_id: josie,
-  duration: 530,
-  start: '2021-04-19 00:00:00',
-  moving_time: 290,
-  start_date_local: '2021-04-19 00:00:00',
-  race_id: 2,
-  race_try_id: 8
-)
+RaceTry.create!([
+  {
+    registration_id: wilford_snowpiercer,
+    user_id: wilford,
+    duration: 500,
+    start: '2021-04-19 00:00:00',
+    moving_time: 260,
+    start_date_local: '2021-04-19 00:00:00',
+    race_id: snowpiercer_race[:id],
+    race_try_id: 1
+  },
+  {
+    registration_id: melanie_snowpiercer,
+    user_id: melanie,
+    duration: 510,
+    start: '2021-04-19 00:00:00',
+    moving_time: 270,
+    start_date_local: '2021-04-19 00:00:00',
+    race_id: snowpiercer_race[:id],
+    race_try_id: 2
+  },
+  {
+    registration_id: layton_snowpiercer,
+    user_id: layton,
+    duration: 520,
+    start: '2021-04-19 00:00:00',
+    moving_time: 280,
+    start_date_local: '2021-04-19 00:00:00',
+    race_id: snowpiercer_race[:id],
+    race_try_id: 3
+  },
+  {
+    registration_id: josie_snowpiercer,
+    user_id: josie,
+    duration: 530,
+    start: '2021-04-19 00:00:00',
+    moving_time: 290,
+    start_date_local: '2021-04-19 00:00:00',
+    race_id: snowpiercer_race[:id],
+    race_try_id: 4
+  },
+  {
+    registration_id: wilford_big_alice,
+    user_id: wilford,
+    duration: 500,
+    start: '2021-04-19 00:00:00',
+    moving_time: 260,
+    start_date_local: '2021-04-19 00:00:00',
+    race_id: big_alice_race[:id],
+    race_try_id: 5
+  },
+  {
+    registration_id: melanie_big_alice,
+    user_id: melanie,
+    duration: 510,
+    start: '2021-04-19 00:00:00',
+    moving_time: 270,
+    start_date_local: '2021-04-19 00:00:00',
+    race_id: big_alice_race[:id],
+    race_try_id: 6
+  },
+  {
+    registration_id: layton_big_alice,
+    user_id: layton,
+    duration: 520,
+    start: '2021-04-19 00:00:00',
+    moving_time: 280,
+    start_date_local: '2021-04-19 00:00:00',
+    race_id: big_alice_race[:id],
+    race_try_id: 7
+  },
+  {
+    registration_id: josie_big_alice,
+    user_id: josie,
+    duration: 530,
+    start: '2021-04-19 00:00:00',
+    moving_time: 290,
+    start_date_local: '2021-04-19 00:00:00',
+    race_id: big_alice_race[:id],
+    race_try_id: 8
+  },
+])
