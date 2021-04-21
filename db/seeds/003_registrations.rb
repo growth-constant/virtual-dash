f0500aa5ff0c1ca2162893022cba4d3c7f2ea3aa
LuisGrwtCnst <luis@growthconstant.com>
2021-04-21 10:13:43 -0600

    DB Seeders - Finished registrations seeder

--- db/seeds/003_registrations.rb
+++ db/seeds/003_registrations.rb
@@ -16,64 +16,64 @@ puts layton[:id]
 # Registrations
 Registration.create!([
   {
-    race_id: snowpiercer_race,
-    user_id: wilford,
+    race_id: snowpiercer_race[:id],
+    user_id: wilford[:id],
     status: 'registered',
     payment_status: 'paid',
     agreements_signed: true,
     session_id: 'wilford-snowpiercer'  
   },
   {
-    race_id: snowpiercer_race,
-    user_id: melanie,
+    race_id: snowpiercer_race[:id],
+    user_id: melanie[:id],
     status: 'registered',
     payment_status: 'paid',
     agreements_signed: true,
     session_id: 'melanie-snowpiercer'
   },
   {
-    race_id: snowpiercer_race,
-    user_id: layton,
+    race_id: snowpiercer_race[:id],
+    user_id: layton[:id],
     status: 'registered',
     payment_status: 'paid',
     agreements_signed: true,
     session_id: 'layton-snowpiercer'
   },
   {
-    race_id: snowpiercer_race,
-    user_id: josie,
+    race_id: snowpiercer_race[:id],
+    user_id: josie[:id],
     status: 'registered',
     payment_status: 'paid',
     agreements_signed: true,
     session_id: 'josie-snowpiercer'
   },
   {
-    race_id: big_alice_race,
-    user_id: wilford,
+    race_id: big_alice_race[:id],
+    user_id: wilford[:id],
     status: 'registered',
     payment_status: 'paid',
     agreements_signed: true,
     session_id: 'wilford-bigalice'
   },
   {
-    race_id: big_alice_race,
-    user_id: melanie,
+    race_id: big_alice_race[:id],
+    user_id: melanie[:id],
     status: 'registered',
     payment_status: 'paid',
     agreements_signed: true,
     session_id: 'melanie-bigalice'
   },
   {
-    race_id: big_alice_race,
-    user_id: layton,
+    race_id: big_alice_race[:id],
+    user_id: layton[:id],
     status: 'registered',
     payment_status: 'paid',
     agreements_signed: true,
     session_id: 'layton-bigalice'
   },
   {
-    race_id: big_alice_race,
-    user_id: josie,
+    race_id: big_alice_race[:id],
+    user_id: josie[:id],
     status: 'registered',
     payment_status: 'paid',
     agreements_signed: true,
