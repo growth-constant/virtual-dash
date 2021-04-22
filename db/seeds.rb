8e7e9879004cf493b298a8f19f1aed935e0684bd
LuisGrwtCnst <luis@growthconstant.com>
2021-04-22 11:12:33 -0600

    DB Seeders - Deleted comments

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,32 +1,5 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
-# user = User.find_by(email: 'victor.j.fdez@gmail.com')
-
-# if user
-#   race = Race.create(
-#     title: 'Test race',
-#     segment_id: '4677383'
-#   )
-
-#   race_category = RaceCategory.create(
-#     title: 'Special category race',
-#     price: 30,
-#     race_id: race.id
-#   )
-
-#   Registration.create(
-#     race_id: race.id,
-#     user_id: user.id,
-#     race_category_id: race_category.id,
-#     status: true
-#   )
-# end
 
 puts 'Reseting PK sequence on all tables..'
 ActiveRecord::Base.connection.tables.each do |t|
