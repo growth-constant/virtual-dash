f3e3d1b802ed19a2d8d53cc9bf43070c02fd9eb1
LuisGrwtCnst <luis@growthconstant.com>
2021-04-22 11:04:39 -0600

    DB Seeders - Added missing image to user seeder

--- db/seeds/001_users.rb
+++ db/seeds/001_users.rb
@@ -10,7 +10,8 @@ User.create!([
     token: '36f2a4c4096d7d446e1f4ccb7018ec476834aaf7',
     token_expires_at: 1617343217,
     refresh_token: 'f3e829dd761c7384bd5da8a096e09d788d8a8423',
-    image: 'https://d3nn82uaxijpm6.cloudfront.net/assets/avatar/athlete/large-800a7033cc92b2a5548399e26b1ef42414dd1a9cb13b99454222d38d58fd28ef.png'
+    image: 'https://d3nn82uaxijpm6.cloudfront.net/assets/avatar/athlete/large-800a7033cc92b2a5548399e26b1ef42414dd1a9cb13b99454222d38d58fd28ef.png',
+    image_medium: 'https://d3nn82uaxijpm6.cloudfront.net/assets/avatar/athlete/medium-bee27e393b8559be0995b6573bcfde897d6af934dac8f392a6229295290e16dd.png'
   },
   {
     name: 'Melanie Cavill',
@@ -21,7 +22,8 @@ User.create!([
     token: '36f2a4c4096d7d446e1f4ccb7018ec476834aaf7',
     token_expires_at: 1617343217,
     refresh_token: 'f3e829dd761c7384bd5da8a096e09d788d8a8423',
-    image: 'https://d3nn82uaxijpm6.cloudfront.net/assets/avatar/athlete/large-800a7033cc92b2a5548399e26b1ef42414dd1a9cb13b99454222d38d58fd28ef.png'
+    image: 'https://d3nn82uaxijpm6.cloudfront.net/assets/avatar/athlete/large-800a7033cc92b2a5548399e26b1ef42414dd1a9cb13b99454222d38d58fd28ef.png',
+    image_medium: 'https://d3nn82uaxijpm6.cloudfront.net/assets/avatar/athlete/medium-bee27e393b8559be0995b6573bcfde897d6af934dac8f392a6229295290e16dd.png'
   },
   {
     name: 'Andre Layton',
@@ -32,7 +34,8 @@ User.create!([
     token: '36f2a4c4096d7d446e1f4ccb7018ec476834aaf7',
     token_expires_at: 1617343217,
     refresh_token: 'f3e829dd761c7384bd5da8a096e09d788d8a8423',
-    image: 'https://d3nn82uaxijpm6.cloudfront.net/assets/avatar/athlete/large-800a7033cc92b2a5548399e26b1ef42414dd1a9cb13b99454222d38d58fd28ef.png'
+    image: 'https://d3nn82uaxijpm6.cloudfront.net/assets/avatar/athlete/large-800a7033cc92b2a5548399e26b1ef42414dd1a9cb13b99454222d38d58fd28ef.png',
+    image_medium: 'https://d3nn82uaxijpm6.cloudfront.net/assets/avatar/athlete/medium-bee27e393b8559be0995b6573bcfde897d6af934dac8f392a6229295290e16dd.png'
   },
   {
     name: 'Josie Wellstead',
@@ -43,6 +46,7 @@ User.create!([
     token: '36f2a4c4096d7d446e1f4ccb7018ec476834aaf7',
     token_expires_at: 1617343217,
     refresh_token: 'f3e829dd761c7384bd5da8a096e09d788d8a8423',
-    image: 'https://d3nn82uaxijpm6.cloudfront.net/assets/avatar/athlete/large-800a7033cc92b2a5548399e26b1ef42414dd1a9cb13b99454222d38d58fd28ef.png'
+    image: 'https://d3nn82uaxijpm6.cloudfront.net/assets/avatar/athlete/large-800a7033cc92b2a5548399e26b1ef42414dd1a9cb13b99454222d38d58fd28ef.png',
+    image_medium: 'https://d3nn82uaxijpm6.cloudfront.net/assets/avatar/athlete/medium-bee27e393b8559be0995b6573bcfde897d6af934dac8f392a6229295290e16dd.png'
   },
 ])
