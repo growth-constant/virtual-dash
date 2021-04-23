38efe702c56e6d78738974cc0a8125c3fe825a63
Luis Angel Ortega <luis@growthconstant.com>
2021-04-23 16:56:48 -0600

    Races page - Working on maps in races page

--- app/javascript/packs/maps/race.js
+++ app/javascript/packs/maps/race.js
@@ -1,32 +1,37 @@
 window.addEventListener('turbolinks:load', function () {
   function initMap() {
-    var data = document.getElementById('map');
-    if (data) {
-      var polyline = data.dataset.polyline;
-      var lat = parseFloat(data.dataset.lat);
-      var lng = parseFloat(data.dataset.lng);
+    var data = document.getElementsByClassName('map-container');
 
-      var map = new google.maps.Map(document.getElementById('map'), {
-        center: {
-          lat: lat, lng: lng,
-        },
-        zoom: 17,
-        mapTypeIds: ['roadmap', 'satellite', 'hybrid', 'terrain', 'styled_map']
+    console.log(data)
+
+    if (data && data.length) {
+      data.forEach( element => {
+        var polyline = data.dataset.polyline;
+        var lat = parseFloat(data.dataset.lat);
+        var lng = parseFloat(data.dataset.lng);
+
+        var map = new google.maps.Map( element, {
+          center: {
+            lat: lat, lng: lng,
+          },
+          zoom: 17,
+          mapTypeIds: ['roadmap', 'satellite', 'hybrid', 'terrain', 'styled_map']
+        });
+
+        var encoded_data = polyline;
+
+        var decode = google.maps.geometry.encoding.decodePath(encoded_data);
+
+        var line = new google.maps.Polyline({
+          path: decode,
+          strokeColor: '#E23510',
+          strokeOpacity: 1.0,
+          strokeWeight: 4,
+          zIndex: 3
+        });
+
+        line.setMap(map);
       });
-
-      var encoded_data = polyline;
-
-      var decode = google.maps.geometry.encoding.decodePath(encoded_data);
-
-      var line = new google.maps.Polyline({
-        path: decode,
-        strokeColor: '#E23510',
-        strokeOpacity: 1.0,
-        strokeWeight: 4,
-        zIndex: 3
-      });
-
-      line.setMap(map);
     }
   }
 
