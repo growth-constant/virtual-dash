770e1ea222190ef2f1dc97121318c3fab988646d
Luis Angel Ortega <luis@growthconstant.com>
2021-05-21 10:07:20 -0600

    Race tries test - Working on test

--- spec/workers/collect_tries_spec.rb
+++ spec/workers/collect_tries_spec.rb
@@ -1,7 +1,18 @@
 require 'spec_helper'
 
 describe CollectTriesJob do
+
+  time = (Time.zone.today + 6.hours).to_datetime
+  scheduled_job = described_class.perform_in(time, 'CollectTriesJob', true)
+
   it "the truth" do
     assert true
   end
+
+  it 'occurs at expected time' do
+    scheduled_job
+    assert_equal true, described_class.jobs.last['jid'].include?(scheduled_job)
+    expect(described_class).to have_enqueued_sidekiq_job('CollectTriesJob', true)
+  end
+
 end
