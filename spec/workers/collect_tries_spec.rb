d01b940d39253d1888599ec541f45f49bde000ad
Luis Angel Ortega <luis@growthconstant.com>
2021-05-24 12:50:01 -0600

    Sidekiq test - Test that job runs at 23:59

--- spec/workers/collect_tries_spec.rb
+++ spec/workers/collect_tries_spec.rb
@@ -2,10 +2,14 @@ require 'spec_helper'
 
 describe CollectTriesJob do
 
-  # time = DateTime.now.change({ hour: 23, min: 59 })
-  
   it 'runs without errors' do
     assert_silent  { CollectTriesJob.perform_now() }
   end
-
+  
+  it 'runs at 23:59' do
+    time = DateTime.now.change({ hour: 23, min: 59 })
+    CollectTriesJob.set(wait_util: time).perform_later()
+    assert_enqueued_with(job: CollectTriesJob)
+  end
+  
 end
