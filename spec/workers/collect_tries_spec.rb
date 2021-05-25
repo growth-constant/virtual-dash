2cce60797f30ac420b3dd862f5832cd5ff8878bc
Luis Angel Ortega <luis@growthconstant.com>
2021-05-25 15:37:23 -0600

    Sidekiq test - Fixed test

--- spec/workers/collect_tries_spec.rb
+++ spec/workers/collect_tries_spec.rb
@@ -1,15 +1,18 @@
-require 'spec_helper'
+require 'rails_helper'
 
-describe CollectTriesJob do
+RSpec.describe CollectTriesJob do
 
   it 'runs without errors' do
     assert_silent  { CollectTriesJob.perform_now() }
   end
   
   it 'runs at 23:59' do
+    ActiveJob::Base.queue_adapter = :test
     time = DateTime.now.change({ hour: 23, min: 59 })
-    CollectTriesJob.set(wait_util: time).perform_later()
-    assert_enqueued_with(job: CollectTriesJob)
+
+    expect {
+      CollectTriesJob.set(:wait_util => time).perform_later
+    }.to enqueue_job(CollectTriesJob)
   end
   
 end
