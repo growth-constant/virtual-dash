198c5fd4a6e86c9b291d157428ec98b0645c9b69
Luis Angel Ortega <luis@growthconstant.com>
2021-06-01 10:26:35 -0600

    Position change mail - Fixed bug on display of the logo

--- app/mailers/race_mailer.rb
+++ app/mailers/race_mailer.rb
@@ -6,6 +6,8 @@ class RaceMailer < ApplicationMailer
     @race = params[:race]
     @place = params[:place]
 
+    attachments.inline['logo.svg'] = File.read('app/assets/images/logo.svg')
+
     mail(
       to: @user[:email],
       subject: "Your position on #{@race[:title]} has changed"
