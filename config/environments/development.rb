378cbf91ef125b47db8cca12b1f4751944513aea
Luis Angel Ortega <luis@growthconstant.com>
2021-06-07 11:33:26 -0600

    Position change mail - Added API key and domain to enviroments

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -46,8 +46,8 @@ Rails.application.configure do
   # Use Mailgun as mailer
   config.action_mailer.delivery_method = :mailgun
   config.action_mailer.mailgun_settings = {
-    api_key: ENV['MAILGUN_API_KEY'],
-    domain: ENV['MAILGUN_API_DOMAIN'],
+    api_key:  ENV['MAILGUN_API_KEY'],
+    domain:  ENV['MAILGUN_API_DOMAIN'],
   }
 
   # Print deprecation notices to the Rails logger.
