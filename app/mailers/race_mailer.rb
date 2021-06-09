0e0914725695d0b6d200a4aabed4dae16423bfde
Luis Angel Ortega <luis@growthconstant.com>
2021-06-09 16:52:56 -0600

    Position change mail - Started race ended mail

--- app/mailers/race_mailer.rb
+++ app/mailers/race_mailer.rb
@@ -13,4 +13,17 @@ class RaceMailer < ApplicationMailer
       subject: "Your position on #{@race[:title]} has changed"
     )
   end
+  
+  def race_ended_email
+    @user = params[:user]
+    @race = params[:race]
+    @place = params[:place]
+    
+    attachments.inline['v-logo.svg'] = File.read('app/assets/images/logos/vertical-logo.svg')
+
+    mail(
+      to: @user[:email],
+      subject: "#{@race[:title]} has ended"
+    )
+  end
 end
--- test/mailers/previews/race_mailer_preview.rb
+++ test/mailers/previews/race_mailer_preview.rb
@@ -13,4 +13,20 @@ class RaceMailerPreview < ActionMailer::Preview
     
     RaceMailer.with(user: user, race: race, place: place).position_change_email
   end
+
+  def race_ended_email
+    user = {
+      :first_name => 'Layton',
+      :email => 'andre@thetail.xyz'
+    }
+    race = {
+      :id => 1,
+      :title => 'Snowpiercer',
+      :startdate => Date.new(2021,5,1),
+      :enddate => Date.new(2021,06,30)
+    }
+    place = '3rd'
+
+    RaceMailer.with(user: user, race: race, place: place).race_ended_email
+  end
 end
