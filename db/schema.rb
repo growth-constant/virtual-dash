d0d2738187275ca5d7984a4f59e2468bdcbcb807
Luis Angel Ortega <luis@growthconstant.com>
2021-07-26 16:26:59 -0600

    Stripe Connect - Added connected account idmigration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_06_172406) do
+ActiveRecord::Schema.define(version: 2021_07_26_221857) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -108,6 +108,7 @@ ActiveRecord::Schema.define(version: 2021_05_06_172406) do
     t.boolean "profile_complete", default: false
     t.date "birthdate"
     t.boolean "is_subscribed", default: false
+    t.string "stripe_conn_acc_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
