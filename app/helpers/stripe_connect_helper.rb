f51ec26d5100312dba67b008469cfecd519192bb
Luis Angel Ortega <luis@growthconstant.com>
2021-07-27 16:50:08 -0600

    Stripe Connect - Added calls to API

--- app/helpers/stripe_connect_helper.rb
+++ app/helpers/stripe_connect_helper.rb
@@ -1,27 +1,51 @@
+require 'stripe'
 
 module StripeConnectHelper
 
+  def thingy(user)
+    connected_acc = create_connect_account(user)
+    add_stripe_conn_acc_id(user, connected_acc.id)
+    linked_acc = create_linked_account(user)
+    redirect_to linked_acc.url
+  end
+
+  private
+
   def create_connect_account(user)
     account = Stripe::Account.create({
-      type: 'express'
+      email: user.email,
+      type: 'express',
+      country: user.country ? user.country : 'US',
+      business_type: 'individual',
+      individual: {
+        first_name: user.first_name,
+        last_name: user.last_name,
+        gender: user.gender,
+        email: user.email,
+        phone: user.phone,
+        address: {
+          city: user.city,
+          country: user.country,
+        },
+        dob: {
+          day: user.birthdate.day,
+          month: user.birthdate.month,
+          year: user.birthdate.year,
+        }
     })
   end
 
   def create_linked_account(user)
     account_links = Stripe::AccountLink.create({
-      account: user.stripe_account_id,
-      refresh_url: 'https://example.com/reauth',
-      return_url: 'https://example.com/return',
+      account: user.stripe_conn_acc_id,
+      refresh_url: 'https://www.virtualdash.co/activity',
+      return_url: 'https://www.virtualdash.co/activity',
       type: 'account_onboarding',
     })
   end
 
-end
+  def add_stripe_conn_acc_id(user, stripe_conn_acc_id)
+    user.update(stripe_conn_acc_id: stripe_conn_acc_id)
+  end
 
-# TODO:
-# - Check all data that is returned from Stripe.
-# - Make a migtration with the new data structure.
-# - Connect the user to the stripe account.
-# - Create return url.
-# - Create refresh url.
-# - Create a new stripe linked account for the user.
+end
