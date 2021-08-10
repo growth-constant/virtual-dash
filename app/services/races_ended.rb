19490351d0b7bd3e3635e715f05cd203f669ce33
Luis Angel Ortega <luis@growthconstant.com>
2021-08-10 12:36:09 -0600

    Stripe Connect - Refactor races ended service

--- app/services/races_ended.rb
+++ app/services/races_ended.rb
@@ -5,27 +5,24 @@ class RacesEnded
   end
 
   def call
-    create_prize_items
-    notify
-  end
-
-  def create_prize_items
     @ended_races.each do | race |
       Prize.create_race_prizes(race)
+      # TODO: Deposit prizes
+      notify(race)
     end
   end
 
-  def notify
-    @ended_races.each do | race |
-      leaderboard = Leaderboard.new(race, :all).call[:competitors]
-      place =  leaderboard.index(leaderboard.find { |try| try[:id] == race.user[:id] })
+  private
 
-      RaceMailer.with(
-        race: race,
-        user: race.user,
-        place: (place + 1).ordinalize
-      ).race_ended_email.deliver_now
-    end
+  def notify(race)
+    leaderboard = Leaderboard.new(race, :all).call[:competitors]
+    place =  leaderboard.index(leaderboard.find { |try| try[:id] == race.user[:id] })
+
+    RaceMailer.with(
+      race: race,
+      user: race.user,
+      place: (place + 1).ordinalize
+    ).race_ended_email.deliver_now
   end
 
 end
